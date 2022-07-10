8e7d3ffc98c52f71b765f79f92135f6a13841022
Masanari Miura <m0s0n0r0h01h01@icloud.com>
2022-07-10 18:09:28 +0900

    修正

--- .history/pages/login_20220710180639.vue
+++ .history/pages/login_20220710180639.vue
@@ -63,7 +63,7 @@ export default {
     const loginUid = {
       uid: this.uid,
     };
-      const resData = await this.$axios.get("http://127.0.0.1:8000/api/v1/user/", loginUid);
+      const resData = await this.$axios.get("http://127.0.0.1:8000/api/v1/user/", { params: { loginUid }});
     this.loginUserData = resData.data.data;
     this.$store.commit('sendLoginUserData', loginUserData);
     this.$router.push('/tweet');
